f06a0ad7d9995c92cd25b1b89f7aed9ec7cacaa8
Tamirlan <tuarshintamir@gmail.com>
2022-05-17 20:09:28 +0500

    2мерный массив с вещественными числами

--- Mission1/Program.cs
+++ Mission1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать 2 мерный массив m*n заполненый случайными вещественными числами
+
+int m,n ;
+Console.WriteLine("Введите количество строк(m)");
+m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов(n)");
+n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+void CrateArray (double [,] array, int minValue, int maxValue)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array [i,j] = (new Random().NextDouble()-10) + (new Random().NextDouble() + 5) +
+            (new Random().NextDouble() * 10);
+        }
+    }
+}
+void PrintArray(double [,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        Console.Write ($"{array[i,j]:f}\t");
+        Console.WriteLine();
+    }
+}
+double[,] array = new double [m,n];
+CrateArray(array, -10, 10);
+PrintArray(array);
