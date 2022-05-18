618b23df6149a8e90d58ba55c317d95b5f06e7c6
Tamirlan <tuarshintamir@gmail.com>
2022-05-18 14:49:10 +0500

    Срденее арифметическое

---
+++ Mission3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+double[,] array = new double[,]
+{
+    {1,4,7,2},
+    {5,9,2,3},
+    {8,4,2,4}
+};
+    for (int i = 3; i < array.GetLength(0); i++)
+    {
+        for (int j = 4; j < array.GetLength(1); j++)
+        Console.WriteLine();
+    }
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j]+ "\t");
+        Console.WriteLine(); 
+    }
+}
+PrintArray(array);
+void Average(double[,] array)
+{
+    for (int j = 0; j < array.GetLength(1);j++)
+    {
+        double res = 0;
+        for (int i = 0; i < array.GetLength(0);i++)
+        {
+            res = res + array[i,j];
+        }
+        Console.WriteLine($"{res/3:f}");
+    }
+}
+Average(array);
